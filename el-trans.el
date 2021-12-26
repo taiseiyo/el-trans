cd8f44a0b3f33122ea74516c06c54dc046787a7a
suzuki taisei <taisei@localhost>
2021-12-26 14:30:44 +0900

    fix

--- el-trans.el
+++ el-trans.el
@@ -1,14 +1,26 @@
-(defun el-trans()
-  "Change the selection range from English to Japanese"
-  (interactive)
-  (setq temp_file (make-temp-file "temp")
-	command (concat "el-trans " temp_file))
+(defun el-trans-main()
   (write-region (region-beginning) (region-end) temp_file)
   (setq trans_text (shell-command-to-string command))
   (setq trans_text (concat "\n" trans_text))
   (goto-char (region-end))
   (insert trans_text)
   (delete-file temp_file)
+  )
+  
+(defun el-trans-en2jp()
+  "Change the selection range from English to Japanese"
+  (interactive)
+  (setq temp_file (make-temp-file "temp")
+	command (concat "el-trans " temp_file " -bt en -at ja"))
+  (el-trans-main)
+  )
+
+(defun el-trans-jp2en()
+  "Change the selection range from English to Japanese"
+  (interactive)
+  (setq command (concat "el-trans " temp_file " -bt ja -at en")
+	temp_file (make-temp-file "temp"))
+  (el-trans-main)
   )
 
 (provide 'el-trans)
